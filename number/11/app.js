636885d5ee773f447954ebb2d7b66d2d208b26a1
olgagrinko-dev <olgagrinko.87@mail.ru>
2022-12-08 18:01:40 +0300

    problem solving

--- number/11/app.js
+++ number/11/app.js
@@ -1,3 +1,20 @@
 // Напишите программу, в которой вычисляется сумма, разность и произведение
 // двух целых чисел, введенных с клавиатуры.
 
+const a = +prompt();
+const b = +prompt();
+if(a % 1 === 0){
+console.log('целое число', true);
+}else {
+    console.log('дробное число', false);
+}
+
+if(b % 1 === 0){
+    console.log('целое число', true);
+    }else {
+        console.log('дробное число', false);
+    }
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
--- number/14/app.js
+++ number/14/app.js
@@ -1,2 +1,9 @@
 //Вы вводите значение с клавиатуры. Если значение четное, то вывести true, в
 // противном случае false
+
+const x = +prompt();
+if (x % 2 === 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
