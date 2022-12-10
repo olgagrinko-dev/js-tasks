419c4a2a1397e3c3bbe8ae9976587ab9a8bdc31b
olgagrinko-dev <olgagrinko.87@mail.ru>
2022-12-10 07:01:49 +0300

    accomplishment of the task

---
+++ String_1/1/app.js
@@ -0,0 +1,15 @@
+// Найти максимальное число из 2 введенных (2 способа)
+
+const a = +prompt();
+const b = +prompt();
+
+console.log(Math.max(a, b))
+
+if (a > b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+
+a > b ? console.log(a) : console.log(b)
+console.log(a > b ? a : b)
---
+++ String_1/3/app.js
@@ -0,0 +1,56 @@
+// Пользователь вводит число от 1 до 7, соответствующее дню недели. Необходимо
+// вывести день для введенного числа.
+// 1 -> Понедельник
+
+const a = +prompt()
+
+if (a === 1) {
+    console.log(`понедельник`);
+} else if (a === 2) {
+    console.log(`вторник`);
+} else if (a === 3) {
+    console.log(`среда`);
+} else if (a === 4) {
+    console.log(`четверг`);
+} else if (a === 5) {
+    console.log(`пятница`);
+} else if (a === 6) {
+    console.log(`суббота`);
+} else if (a === 7) {
+    console.log(`воскресенье`);
+} else {
+    console.log(`ошибка ввода`);
+}
+switch (a) {
+    case 1:
+        console.log(`понедельник`);
+        break;
+
+    case 2:
+        console.log(`вторник`);
+        break;
+
+    case 3:
+        console.log(`среда`);
+        break;
+
+    case 4:
+        console.log(`четверг`);
+        break;
+
+    case 5:
+        console.log(`пятница`);
+        break;
+
+    case 6:
+        console.log(`суббота`);
+        break;
+
+    case 7:
+        console.log(`воскресенье`);
+        break;
+
+    default: console.log(`ошибка ввода`)
+        break;
+
+}
