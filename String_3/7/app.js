05373f7af7362b41f47d37ff5cd896d2b70769b3
olgagrinko-dev <olgagrinko.87@mail.ru>
2022-12-17 19:26:11 +0300

    problem solving

--- String_3/7/app.js
+++ String_3/7/app.js
@@ -1,4 +1,7 @@
 // На вход подается строка `/home/user/dir/file.txt`. Необходимо вернуть имя файла
 // (подстрока после последнего символа `/` ) из полного пути к файлу('file.txt')
 
-const str = '/home/user/dir/file.txt'
+const str1 = '/home/user/dir/file.txt';
+const str2 = str1.indexOf('file.txt');
+console.log(str1.slice(str2));
+
