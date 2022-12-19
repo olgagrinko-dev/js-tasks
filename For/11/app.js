fbbf0096a4cef09d6dc2b8a113899413b900f2bb
olgagrinko-dev <olgagrinko.87@mail.ru>
2022-12-19 16:24:26 +0300

    problem solving

--- For/11/app.js
+++ For/11/app.js
@@ -2,5 +2,13 @@
 // проверьте есть ли в массиве элемент со значением, равным 4. Посчитать
 // количество повторений числа 4 в массиве
 
-const str = prompt().trim();
+const arr = [1, 2, 5, 9, 4, 13, 4, 10];
+let result = 0
 
+for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] === 4) {
+        result += 1
+    }
+}
+console.log(result);
--- For/8/app.js
+++ For/8/app.js
@@ -1,5 +1,11 @@
 // Дан массив с элементами [2, 3, 4, 5]. С помощью цикла for найдите произведение
 // элементов этого массива
 
-const str = prompt().trim();
+const arr = [2, 3, 4, 5];
+let result = 1;
 
+for (let i = 0; i < arr.length; i++) {
+   
+    result *= arr[i]
+}
+console.log(result);
